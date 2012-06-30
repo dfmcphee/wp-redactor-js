7da97941bddfcfdbfaaa4ac9767acb00401d6a60
Dominic McPhee <dominic.mcphee@gmail.com>
2012-06-29 23:32:25 -0400

    Adding updater code and read me,

--- plugin.php
+++ plugin.php
@@ -9,7 +9,24 @@ Author: Dominic McPhee
 Author URI: http://dominicmcphee.comww
 */
 
+require_once('updater.php');
 require_once('redactor.inc.php');
+
+if (is_admin()) { // note the use of is_admin() to double check that this is happening in the admin
+    $config = array(
+        'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
+        'proper_folder_name' => 'redactor-js', // this is the name of the folder your plugin lives in
+        'api_url' => 'https://api.github.com/repos/dfmcphee/wp-redactor-js', // the github API url of your github repo
+        'raw_url' => 'https://raw.github.com/dfmcphee/wp-redactor-jsmaster', // the github raw url of your github repo
+        'github_url' => 'https://github.com/dfmcphee/wp-redactor-js', // the github url of your github repo
+        'zip_url' => 'https://github.com/dfmcphee/wp-redactor-js/zipball/master', // the zip url of the github repo
+        'sslverify' => true // wether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
+        'requires' => '3.0', // which version of WordPress does your plugin require?
+        'tested' => '3.4.1', // which version of WordPress is your plugin tested up to?
+        'readme' => 'README.MD' // which file to use as the readme for the version number
+    );
+    new WPGitHubUpdater($config);
+}
 
 /**
  * Enqueue stylesheets
---
+++ readme.md
@@ -0,0 +1,5 @@
+# RedactorJS.com WordPress Plugin #
+
+~Current Version:0.1.0~
+
+Replaces the default WordPress editor (TinyMCE) with the much more elgant Redactor (redactorjs.com). Redactor is free to use on non-profit sites, but you do have to purchase a license if you use it for commercial projects.
