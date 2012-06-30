265d6bb4d96505fa70157c778c3aaa2de8d9f313
Dominic McPhee <dominic.mcphee@gmail.com>
2012-06-30 00:27:30 -0400

    Updates to updater.

--- plugin.php
+++ plugin.php
@@ -4,32 +4,123 @@
 Plugin Name: RedactorJS.com
 Plugin URI: http://dominicmcphee.com/redactor-wp-editor
 Description: Replaces the default WordPress editor (TinyMCE) with the much more elgant Redactor (redactorjs.com). Redactor is free to use on non-profit sites, but you do have to purchase a license if you use it for commercial projects.
-Version: 0.2.0
+Version: 0.1.2
 Author: Dominic McPhee
 Author URI: http://dominicmcphee.comww
 */
 
-require_once('updater.php');
+require_once('redactor.inc.php');
 
-function github_plugin_updater_test_init() {
-    include_once('redactor.inc.php');
-    if (is_admin()) { // note the use of is_admin() to double check that this is happening in the admin
-        $config = array(
-            'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
-            'proper_folder_name' => 'redactor-js', // this is the name of the folder your plugin lives in
-            'api_url' => 'https://api.github.com/repos/dfmcphee/wp-redactor-js', // the github API url of your github repo
-            'raw_url' => 'https://raw.github.com/dfmcphee/wp-redactor-js/master', // the github raw url of your github repo
-            'github_url' => 'https://github.com/dfmcphee/wp-redactor-js', // the github url of your github repo
-            'zip_url' => 'https://github.com/dfmcphee/wp-redactor-js/zipball/master', // the zip url of the github repo
-            'sslverify' => false ,// wether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
-            'requires' => '3.0', // which version of WordPress does your plugin require?
-            'tested' => '3.4.1', // which version of WordPress is your plugin tested up to?
-            'readme' => 'README.md' // which file to use as the readme for the version number
-        );
-        new WPGitHubUpdater($config);
+define('DEBUG', false);
+
+class WpPluginAutoUpdate {
+    # URL to check for updates, this is where the index.php script goes
+    public $api_url;
+
+    # Type of package to be updated
+    public $package_type;
+
+    public $plugin_slug;
+    public $plugin_file;
+
+    public function WpPluginAutoUpdate($api_url, $type, $slug) {
+        $this->api_url = $api_url;
+        $this->package_type = $type;
+        $this->plugin_slug = $slug;
+        $this->plugin_file = $slug .'/'. $slug . '.php';
     }
-    add_action('init', 'github_plugin_updater_test_init');
+
+    public function print_api_result() {
+        print_r($res);
+        return $res;
+    }
+
+    public function check_for_plugin_update($checked_data) {
+        if (empty($checked_data->checked))
+            return $checked_data;
+        
+        $request_args = array(
+            'slug' => $this->plugin_slug,
+            'version' => $checked_data->checked[$this->plugin_file],
+            'package_type' => $this->package_type,
+        );
+
+        $request_string = $this->prepare_request('basic_check', $request_args);
+        
+        // Start checking for an update
+        $raw_response = wp_remote_post($this->api_url, $request_string);
+
+        if (!is_wp_error($raw_response) && ($raw_response['response']['code'] == 200)) {
+            $response = unserialize($raw_response['body']);
+
+            if (is_object($response) && !empty($response)) // Feed the update data into WP updater
+                $checked_data->response[$this->plugin_file] = $response;
+        }
+        
+        return $checked_data;
+    }
+
+    public function plugins_api_call($def, $action, $args) {
+        if ($args->slug != $this->plugin_slug)
+            return false;
+        
+        // Get the current version
+        $plugin_info = get_site_transient('update_plugins');
+        $current_version = $plugin_info->checked[$this->plugin_file];
+        $args->version = $current_version;
+        $args->package_type = $this->package_type;
+        
+        $request_string = $this->prepare_request($action, $args);
+        
+        $request = wp_remote_post($this->api_url, $request_string);
+        
+        if (is_wp_error($request)) {
+            $res = new WP_Error('plugins_api_failed', __('An Unexpected HTTP Error occurred during the API request.</p> <p><a href="?" onclick="document.location.reload(); return false;">Try again</a>'), $request->get_error_message());
+        } else {
+            $res = unserialize($request['body']);
+            
+            if ($res === false)
+                $res = new WP_Error('plugins_api_failed', __('An unknown error occurred'), $request['body']);
+        }
+        
+        return $res;
+    }
+
+    public function prepare_request($action, $args) {
+        $site_url = site_url();
+
+        $wp_info = array(
+            'site-url' => $site_url,
+            'version' => $wp_version,
+        );
+
+        return array(
+            'body' => array(
+                'action' => $action, 'request' => serialize($args),
+                'api-key' => md5($site_url),
+                'wp-info' => serialize($wp_info),
+            ),
+            'user-agent' => 'WordPress/' . $wp_version . '; ' . get_bloginfo('url')
+        );
+    }
 }
+
+$wp_plugin_auto_update = new WpPluginAutoUpdate('http://www.dominicmcphee.com/plugins/redactor-js/', 'stable', basename(dirname(__FILE__)));
+
+if (DEBUG) {
+    // Enable update check on every request. Normally you don't need 
+    // this! This is for testing only!
+    set_site_transient('update_plugins', null);
+
+    // Show which variables are being requested when query plugin API
+    add_filter('plugins_api_result', array($wp_plugin_auto_update, 'print_api_result'), 10, 3);
+}
+
+// Take over the update check
+add_filter('pre_set_site_transient_update_plugins', array($wp_plugin_auto_update, 'check_for_plugin_update'));
+
+// Take over the Plugin info screen
+add_filter('plugins_api', array($wp_plugin_auto_update, 'plugins_api_call'), 10, 3);
 
 /**
  * Enqueue stylesheets
